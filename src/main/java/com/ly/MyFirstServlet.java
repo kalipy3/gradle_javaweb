6eb98860dd3ca431c2eb7e85727e0d7181d965ae
kalipy <3069087972@qq.com>
2020-09-17 23:08:48 +0800

    ConfigServlet.java ContextServlet.java

--- src/main/java/com/ly/MyFirstServlet.java
+++ src/main/java/com/ly/MyFirstServlet.java
@@ -1,6 +1,7 @@
 package com.ly;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -11,35 +12,62 @@ import javax.servlet.ServletResponse;
  * Copyright (C) 2020 kalipy <kalipy@debian>
  *
  * Distributed under terms of the MIT license.
- * 一个Servlet只能处理一个指定请求
- * 应该指定servlet处理哪个请求
- * 需要在web.xml里配置servlet的详细信息
+ * Servlet的生命周期
+ *
+ * Servlet是跑在服务器上的
+ *Tomcat服务器--->Servlet容器
+ *
+ * 生命周期：从出生到死亡的过程
+ * Servlet的生命周期：Servlet从创建到销毁的过程
+ *
+ * 当我们第一次访问MyFirstServlet时：
+ *  1.创建一个Servlet对象
+ *  2.调用Init方法
+ *  3.调用Service方法
+ *
+ *以后请求：
+    4.只调用Service方法来处理请求 整个运行期间只创建一个servlet对象
+        servlet是单实例，多线程运行。
+
+  当项目从服务器卸载：
+    5.服务器会调用destroy方法
+ *
  */
 public class MyFirstServlet implements Servlet {
 
+    public MyFirstServlet() {
+        System.out.println("我是MyFirstServlet构造方法..");
+    }
+
     @Override
     public void destroy() {
+        System.out.println("我是destroy方法..");
     }
 
     @Override
     public ServletConfig getServletConfig() {
+        System.out.println("我是getServletConfig方法..");
         return null;
     }
 
     @Override
     public String getServletInfo() {
+        System.out.println("我是getServletInfo方法..");
         return null;
     }
 
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("我是init方法..");
     }
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("我是地一个Servlet");
+        System.out.println("我是service方法..");
         PrintWriter writer = servletResponse.getWriter();
         writer.write("HelloWorld");
     }
+
+
 }
 
