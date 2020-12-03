04834ed2c59b986ef6f24758bef6cf8dc9e2333b
kalipy <3069087972@qq.com>
2020-12-03 19:25:08 +0800

    订单管理 编写serviceimpl 03

--- src/main/java/com/service/OrderItemServiceImpl.java
+++ src/main/java/com/service/OrderItemServiceImpl.java
@@ -1,4 +1,9 @@
 package com.service;
+
+import java.util.List;
+import com.dao.OrderItemDao;
+import com.dao.OrderItemDaoImpl;
+import com.bean.OrderItem;
 /*
  * OrderItemServiceImpl.java
  * Copyright (C) 2020 2020-12-03 19:02 kalipy <kalipy@debian>
@@ -8,20 +13,18 @@ package com.service;
 
 public class OrderItemServiceImpl implements OrderItemService
 {
-	public OrderItemServiceImpl() {
-		
-	}
-    
+   OrderItemDao itemDao = new OrderItemDaoImpl();
+
     //保存订单项
     @Override
     public void saveItem(OrderItem orderItem) {
-
+        itemDao.saveOrderItem(orderItem);
     }
 
     //获取订单的所有订单项
     @Override
     public List<OrderItem> getOrderItems(String orderid) {
-
+        return itemDao.getOrderItems(orderid);
     }
 }
 
--- src/main/java/com/service/OrderServiceImpl.java
+++ src/main/java/com/service/OrderServiceImpl.java
@@ -1,4 +1,11 @@
 package com.service;
+
+import java.util.List;
+
+import com.bean.Cart;
+import com.bean.Order;
+import com.dao.OrderDao;
+import com.dao.OrderDaoImpl;
 /*
  * OrderServiceImpl.java
  * Copyright (C) 2020 2020-12-03 18:59 kalipy <kalipy@debian>
@@ -8,26 +15,39 @@ package com.service;
 
 public class OrderServiceImpl implements OrderService
 {
-    public OrderServiceImpl() {
-
-    }
+    OrderDao orderDao = new OrderDaoImpl();
 
     //结账
     @Override
     public String checkout(Cart cart) {
-
+        return null;
     }
 
     //修改订单状态
     @Override
-    public void updateStatus(String orderid, String status){}
+    public void updateStatus(String orderid, String status){
+        Order order = new Order();
+        order.setOrderId(orderid);
+        int parseInt = 0;
+        try {
+            parseInt = Integer.parseInt(status);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        order.setStatus(parseInt);
+        orderDao.updateStatus(order);
+    }
 
     //获取所有订单，管理员用
     @Override
-    public List<Order> getAllOrder(){}
+    public List<Order> getAllOrder(){
+        return orderDao.getOrderList();
+    }
 
     //获取某个用户的所有订单
     @Override
-    public List<Order> getMyOrders(Integer userId){}
+    public List<Order> getMyOrders(Integer userId){
+        return orderDao.getOrderByUserId(userId);
+    }
 }
 
