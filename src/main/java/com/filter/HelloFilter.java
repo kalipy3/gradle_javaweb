dbc68185e7436ce7bffb616beb0ad8eb9121f315
kalipy <3069087972@qq.com>
2020-12-04 18:50:56 +0800

    filter03 生命周期

--- src/main/java/com/filter/HelloFilter.java
+++ src/main/java/com/filter/HelloFilter.java
@@ -22,16 +22,25 @@ import javax.servlet.ServletResponse;
 //
 //1.写一个现实
 //2.在服务器配置filter要过滤哪些请求(web.xml)
+
+//服务器管理的filter,生命周期
+//从创建到销毁的过程
+//1.创建-初始化
+//      服务器一启动,项目加载进服务器,创建filter对象，并执行初始化,单例多线程
+//2.每次拦截都执行
+//      doFilter()
+//3.销毁
+//      项目从服务器中卸载
 public class HelloFilter implements Filter
 {
     @Override 
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        System.out.println("filter init()..");
     }
     
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        System.out.println("我是hello.jsp的拦截器");
+        System.out.println("filter doFilter()..");
         String money = req.getParameter("money");
         if (money != null) {
             //带了钱，让她访问vtuber页面
@@ -42,13 +51,17 @@ public class HelloFilter implements Filter
             resp.getWriter().write("HelloFilter..");
         } else {
             //没带钱，提示你回家要钱去
-            resp.getWriter().write("no money, no mm!");
+            //resp.getWriter().write("no money, no mm!");
         }
     }
 
     @Override 
     public void destroy() {
+        System.out.println("filter destroy()..");
+    }
 
+    public HelloFilter() {
+        System.out.println("filter HelloFilter()构造器..");
     }
 }
 
