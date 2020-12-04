fff4d39970cb5b0b3be7144ba45e6b049884b8df
kalipy <3069087972@qq.com>
2020-12-04 19:38:49 +0800

    filter07 在代码中过滤拦截部分请求

--- src/main/java/com/filter/Afilter.java
+++ src/main/java/com/filter/Afilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 /*
  * Afilter.java
@@ -24,8 +25,20 @@ public class Afilter implements Filter
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String uri = req.getRequestURI();
+        StringBuffer url = req.getRequestURL();
+        System.out.println("过来的请求uri:" + uri);
+        System.out.println("过来的请求url:" + url.toString());
         System.out.println("我是Afilter..doFilter()..");
+
+        //只拦截page下的所有jsp 虽然web.xml配置会拦截page下的所有
+        if (uri.endsWith("jsp")) {
+            System.out.println("jsp结尾的，我就拦截了");
+        } else {
+            chain.doFilter(request, response);
+        }
     }
 
     @Override 
